db62105573555bbda73481a91abaf7003e9ae153
Elton <elton.ribeiro@agrotis.com>
2020-06-14 10:52:39 -0300

    Mais rotas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,6 +3,22 @@ import knex from './database/connection';
 
 const routes = express.Router();
 
+//Função para calcular média de estrelas
+async function calcularMediaEstrelas(id_estabelecimento: Number) {
+    try{
+        let avaliacoes = await knex('avaliacao').where('id_estabelecimento', id_estabelecimento).select('estrelas');
+        let counter = 0;
+        for (let i in avaliacoes) {
+            counter += Number(avaliacoes[i].estrelas);
+        }
+        const qtdeAvaliacoes = avaliacoes.length;
+        const media = counter / qtdeAvaliacoes;
+        return {media: media, qtdeAvaliacoes: qtdeAvaliacoes};
+    } catch(e) {
+        return e;
+    }
+}
+
 //LISTAR TODOS OS CAMINHONEIROS
 routes.get('/listar-caminhoneiros', async (request, response) => {
         const userData = await knex('caminhoneiros').select('*');
@@ -135,23 +151,25 @@ routes.get('/caminhoneiro/:id', async (request, response) => {
 //EXCLUIR CONTA CAMINHONEIRO
 routes.delete('/caminhoneiro/:id', async (request, response) => {
     let id = request.params.id;
+    const trx = await knex.transaction();
 
     try {
-        const userData = await knex('caminhoneiros').where('id_caminhoneiros', id).delete();
-        if (!userData) {
+        const dadosParaApagar = await trx('caminhoneiros').where('id_caminhoneiros', id).delete();
+        //apagando comentarios vinculados
+        trx('historico_comentarios').where('id_caminhoneiro', id).delete();
+        //apagando avaliacoes vinculadas
+        trx('avaliacao').where('id_estabelecimento', id).delete();
+        if (!dadosParaApagar) {
+            trx.rollback();
             return (response.status(400).json({ message: 'Erro, usuário inexistente!'}));
         };
+        trx.commit();
         return (response.status(200).json({ message: 'Cadastro excluído com sucesso!'}));
     } catch (e){
+        trx.rollback();
         return ( response.status(400).json({ message: 'Erro na exclusão, tente novamente mais tarde.'}));
     }
 });
-
-
-//CRIAR TABELA COMENTARIOS COM RELACAO CAMINHONEIROS / ESTABELECIMENTOS
-//
-
-//CRIAR TABELA AVALIACOES COM RELACAO CAMINHONEIROS / ESTABELECIMENTOS
 
 //CADASTRAR ESTABELECIMENTO
 routes.post('/cadastro-estabelecimento', async (request, response) => {
@@ -169,10 +187,16 @@ routes.post('/cadastro-estabelecimento', async (request, response) => {
         banheiro: request.body.banheiro, 
         estacionamento: request.body.estacionamento, 
         descricao: request.body.descricao, 
-        created: new Date(), 
-        modified: null,
         senha: request.body.senha,
-        autenticado: 1
+        autenticado: 1,
+        img_url: request.body.img_url,
+        banho: request.body.banho,
+        combustivel: request.body.combustivel,
+        raio_dez_km: request.body.raio_dez_km,
+        aberto_24h: request.body.aberto_24h,
+        refeicao: request.body.refeicao,
+        cafe: request.body.cafe,
+        saude: request.body.saude
     };
     try {
         await knex('estabelecimentos').insert(dadosCadastro);
@@ -182,8 +206,8 @@ routes.post('/cadastro-estabelecimento', async (request, response) => {
     }
 });
 
-//LISTAR TODOS OS ESTABELECIMENTOS
-routes.get('/estabelecimentos', async (request, response) => {
+//LISTAR TODOS OS ESTABELECIMENTOS SEM FILTRO
+routes.get('/estabelecimentos-todos', async (request, response) => {
     try {
         const todosEstabelecimentos = await knex('estabelecimentos');
 
@@ -197,20 +221,14 @@ routes.get('/estabelecimentos', async (request, response) => {
 
 });
 
-//LISTAR DADOS DO ESTABELECIMENTO POR ID AO SELECIONAR NA LISTA
+//LISTAR TODOS OS DADOS DO ESTABELECIMENTO POR ID AO SELECIONAR NA LISTA
 routes.get('/estabelecimento/:id', async (request, response) => {
-    let id = request.params.id;
+    let id = Number(request.params.id);
     try {
         const estabelecimentoSelecionado = await knex('estabelecimentos').where('id', id)
 
         const comentarios = await knex('historico_comentarios').where('id_estabelecimento', id).select('id_caminhoneiro', 'comentario');
-        let avaliacoes = await knex('avaliacao').where('id_estabelecimento', id).select('estrelas');
-        let counter = 0;
-        for (let i in avaliacoes) {
-            counter += Number(avaliacoes[i].estrelas);
-        }
-        const qtdeComentarios = avaliacoes.length;
-        const media = counter / qtdeComentarios;
+        let avaliacoes = await calcularMediaEstrelas(id);
 
         if(!estabelecimentoSelecionado) {
             return (response.status(400).json({ message: 'Erro, estabelecimento inexistente!'}));
@@ -232,8 +250,9 @@ routes.get('/estabelecimento/:id', async (request, response) => {
                 banheiro:item.banheiro,
                 saude: item.saude,
                 comentarios: comentarios,
-                avaliacao: media,
-                quantidade_comentarios: qtdeComentarios
+                qtdeComentarios: comentarios.length,
+                avaliacao: avaliacoes.media,
+                qtdeAvaliacoes: avaliacoes.qtdeAvaliacoes
             }
         })
         return (response.status(200).json(serialized));
@@ -242,9 +261,119 @@ routes.get('/estabelecimento/:id', async (request, response) => {
     }
 });
 
+//LISTAR ESTABELECIMENTOS COM FILTROS
+routes.get('/estabelecimentos', async (request, response) => {
+    //limite de resultados definido na url de busca
+    let limite = Number(request.query.limite)
+    //offset é para fazer paginação, exemplo, se eu quiser que a busca traga os proximos 4 resultados, coloco offset=4... ele pula os 4 primeiros
+    let offset = Number(request.query.offset)
+
+    //parametros da busca
+    const params = {
+        combustivel: request.query.combustivel,
+        aberto_24h: request.query.aberto_24h,
+        banho: request.query.banho,
+        raio_dez_km: request.query.raio_dez_km,
+        wifi: request.query.wifi,
+        estacionamento: request.query.estacionamento,
+        refeicao: request.query.refeicao,
+        cafe: request.query.cafe,
+        banheiro:request.query.banheiro,
+        saude: request.query.saude
+    }
+    try {
+        const estabelecimentosFiltrados = await knex('estabelecimentos').where(params).offset(offset).limit(limite)
+        if(!estabelecimentosFiltrados) {
+            return (response.status(400).json({ message: 'Erro, estabelecimento inexistente!'}));
+        }
+        let serializedToEdition = {};
+        const serialized = estabelecimentosFiltrados.map(async item => {
+            const avaliacao = await calcularMediaEstrelas(item.id);
+            serializedToEdition =  {
+                id_estabelecimento: item.id,
+                img_url: String(item.img_url).split(','),  //split() pra separar por vírgula e montar uma array
+                endereco: item.endereco,
+                cep: String(item.cep).replace(/^(\d{2})(\d{3})(\d{3}).*/, '$1.$2-$3'), //Regex pra colocar mascara no cep
+                combustivel: item.combustivel,
+                aberto_24h: item.aberto_24h,
+                banho: item.banho,
+                raio_dez_km: item.raio_dez_km,
+                wifi: item.wifi,
+                estacionamento: item.estacionamento,
+                refeicao: item.refeicao,
+                cafe: item.cafe,
+                banheiro:item.banheiro,
+                saude: item.saude,
+                media: avaliacao.media,
+                qtdeAvaliacoes: avaliacao.qtdeAvaliacoes
+            };
+        });
+        return (response.status(200).json(serializedToEdition));
+    } catch (e){
+        return ( response.status(400).json({ message: 'Erro na consulta, tente novamente mais tarde.'}));
+    }
+});
+
+//APAGAR ESTABELECIMENTO
+routes.delete('/estabelecimento/:id', async (request, response) => {
+    let id = request.params.id;
+    const trx = await knex.transaction();
+
+    try {
+        //apagando estabelecimento
+        const dadosParaApagar = await trx('estabelecimentos').where('id', id).delete();
+        //apagando comentarios vinculados
+        trx('historico_comentarios').where('id_estabelecimento', id).delete();
+        //apagando avaliacoes vinculadas
+        trx('avaliacao').where('id_estabelecimento', id).delete();
+        
+        if (!dadosParaApagar) {
+            trx.rollback();
+            return (response.status(400).json({ message: 'Estabelecimento inexistente!'}));
+        };
+        trx.commit();
+        return (response.status(200).json({ message: 'Cadastro excluído com sucesso!'}));
+    } catch (e){
+        trx.rollback();
+        return ( response.status(400).json({ message: 'Erro na exclusão, tente novamente mais tarde.'}));
+    }
+});
+
 //LISTAR ESTABELECIMENTOS PRÓXIMOS
 
 //ATUALIZAR DADOS ESTABELECIMENTO
+routes.put('/estabelecimento/:id', async (request, response) => {
+    let id = request.params.id;
+    
+    const dadosUpdate = {
+        nome: request.body.nome, 
+        email: request.body.email, 
+        endereco: request.body.endereco, 
+        cidade: request.body.cidade, 
+        cnpj: request.body.cnpj, 
+        cep: request.body.cep, 
+        uf: request.body.uf, 
+        ddd: request.body.ddd, 
+        celular: request.body.celular, 
+        wifi: request.body.wifi, 
+        banheiro: request.body.banheiro, 
+        estacionamento: request.body.estacionamento, 
+        descricao: request.body.descricao, 
+        modified: new Date(),
+        senha: request.body.senha,
+        autenticado: 1
+    };
+    try {
+        const dados = await knex('estabelecimentos').where('id', id).update(dadosUpdate);
+        if (!dados) {
+            return response.status(400).json({ erro: 'cadastro inexistente'});
+        }
+        
+    } catch (e){
+        return ( response.status(400).json({ message: 'Erro na atualização, por favor tente novamente e verifique seus dados.'}));
+    }
+    return response.status(200).json({ message: 'Alterações realizadas com sucesso!'});
+});
 
 //EXCLUIR CONTA ESTABELECIMENTO
 
